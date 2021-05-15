303b3e6feb7b8d57f4a0cdd102db2ddb3d4fe47a
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-05-16 00:22:30 +0300

    Create tkinter.py

---
+++ tkinter.py
@@ -0,0 +1,7 @@
+from tkinter import * 
+window = Tk()
+window.title("welcome")
+lbl = Label(window, text="11.12.20 <3 ", font=("Arial Bold", 30))
+lbl.grid(column=0, row=0)
+window.geometry("290x200")
+window.mainloop()
